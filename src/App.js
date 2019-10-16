f9a4f1a04b525e368bf0e44d73b8bdd26e357ff6
alec blakeley <alecblkly@gmail.com>
2019-10-16 13:43:38 -0400

    Updated header

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>Updating header for Breakout Room 🤯</h1>
       <h2>
         Deployed with{' '}
         <a
